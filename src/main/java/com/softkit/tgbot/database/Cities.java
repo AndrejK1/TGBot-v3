c2ab8489057af1942a8c9f2c19196aef1e8b1013
saloed <bogdan19021998ua@gmail.com>
2019-12-11 16:01:48 +0200

    Fill table Cities. Withot links to other tables.

--- src/main/java/com/softkit/tgbot/database/Cities.java
+++ src/main/java/com/softkit/tgbot/database/Cities.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.sql.Timestamp;
+import java.util.Objects;
 
 @Entity
 public class Cities {
@@ -13,4 +13,38 @@ public class Cities {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long cityId;
 
+    private String cityName;
+
+    public Cities(){
+    }
+
+    public Cities( String cityName )
+    {
+        this.cityName = cityName;
+    }
+
+    public long getCityId() {
+        return cityId;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cities)) return false;
+        Cities cities = (Cities) o;
+        return cityId == cities.cityId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cityId);
+    }
 }
