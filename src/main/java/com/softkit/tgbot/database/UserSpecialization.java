d8188ae80630e4cdbcb80cf6444133a86fb20edf
saloed <bogdan19021998ua@gmail.com>
2019-12-11 17:35:46 +0200

    Fill table UserSpecialization. Without links to other tables.

--- src/main/java/com/softkit/tgbot/database/UserSpecialization.java
+++ src/main/java/com/softkit/tgbot/database/UserSpecialization.java
@@ -1,4 +1,46 @@
 package com.softkit.tgbot.database;
 
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
 public class UserSpecialization {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int specializationId;
+
+    private String specializationDescription;
+
+    public UserSpecialization() {
+    }
+
+    public UserSpecialization( String specializationDescription ) {
+        this.specializationDescription = specializationDescription;
+    }
+
+    public int getSpecializationId() {
+        return specializationId;
+    }
+
+    public String getSpecializationDescription() {
+        return specializationDescription;
+    }
+
+    public void setSpecializationDescription(String specializationDescription) {
+        this.specializationDescription = specializationDescription;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserSpecialization)) return false;
+        UserSpecialization that = (UserSpecialization) o;
+        return specializationId == that.specializationId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(specializationId);
+    }
 }
