63db87b892e72ad16a59eda2b1f6aafff37b8953
saloed <bogdan19021998ua@gmail.com>
2019-12-11 16:53:44 +0200

    Fill table UserEmployment. Without links to other tables.

--- src/main/java/com/softkit/tgbot/database/UserEmployment.java
+++ src/main/java/com/softkit/tgbot/database/UserEmployment.java
@@ -1,4 +1,53 @@
 package com.softkit.tgbot.database;
 
+import org.hibernate.annotations.Type;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
 public class UserEmployment {
+
+    @Id
+    @Column( unique = true )
+    private long telegramId;
+
+    @Type( type = "int-array" )
+    @Column(
+            columnDefinition = "integer[]"
+    )
+    private int [] arrayEmployments;
+
+    public UserEmployment() {
+    }
+
+    public UserEmployment(long telegramId ) {
+        this.telegramId = telegramId;
+    }
+
+    public long getTelegramId() {
+        return telegramId;
+    }
+
+    public int[] getArrayEmployments() {
+        return arrayEmployments;
+    }
+
+    public void setArrayEmployments(int[] arrayEmployments) {
+        this.arrayEmployments = arrayEmployments;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserEmployment)) return false;
+        UserEmployment that = (UserEmployment) o;
+        return telegramId == that.telegramId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(telegramId);
+    }
 }
